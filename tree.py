4fbf6c6ec226d18009cc51128b6342fe665e1de1
Karina-00 <karina1szubert@gmail.com>
2020-04-08 00:25:07 +0200

    deleting works

--- tree.py
+++ tree.py
@@ -8,6 +8,7 @@ class Node:
         self.val = nodeValue
         self.left = None
         self.right = None
+        self.parent = None
 
     def __repr__(self):
         return f"\x1b[1m{self.val}\x1b[0m[\x1b[31m{str(self.left)}\x1b[0m] \x1b[1m{self.val}\x1b[0m[\x1b[32m{str(self.right)}\x1b[0m]"
@@ -51,17 +52,96 @@ class Tree:
             head = head.right
         return head
 
+    def path_to_min(self):
+        if not self.root:
+            return []
+        alist = [self.root.val]
+        node = self.root
+        while node.left:
+            alist.append(node.left.val)
+            node = node.left
+        return alist
+
+    def path_to_max(self):
+        if not self.root:
+            return []
+        alist = [self.root.val]
+        node = self.root
+        while node.right:
+            alist.append(node.right.val)
+            node = node.right
+        return alist
+
     def find(self, nodeKey):
         head = self.root
         while head:
-            print(head)
+            print(head.val)
             if nodeKey == head.val:
                 return head
             if nodeKey < head.val:
                 head = head.left
             elif nodeKey > head.val:
                 head = head.right
-        return None
+        return head
+
+    def remove(self, node):
+        if not node:
+            return None
+        node_parent = node.parent
+        child_num = bool(node.left) + bool(node.right)
+        if child_num == 0:
+            if node_parent:
+                if node_parent.left == node:
+                    node_parent.left = None
+                else:
+                    node_parent.right = None
+            else:
+                self.root = None
+
+        elif child_num == 1:
+            child = node.left if node.left else node.right
+            if node_parent:
+                if node_parent.left == node:
+                    node_parent.left = child
+                else:
+                    node_parent.right = child
+            else:
+                self.root = child
+            child.parent = node_parent
+
+        elif child_num == 2:
+            new_node = node.right
+            while new_node.left:
+                new_node = new_node.left
+            node.value = new_node.val
+            self.remove(new_node)
+
+    def delete(self):
+        n = input("Ile kluczy chcesz usunąć?")
+        try:
+            n = int(n)
+        except ValueError:
+            print("Należy podać liczbę")
+            self.delete()
+        n = int(n)
+        if n == 0:
+            return None
+        keys = []
+        print("Podaj klucze:")
+        for i in range(n):
+            try:
+                keys.append(int(input(f"{i + 1}. ")))
+            except ValueError:
+                print("Należy podać liczbę")
+                self.delete()
+        for key in keys:
+            x = self.find(key)  # zwraca caly korzen pod tym elementem
+            if x:
+                print("key", x)
+                self.remove(x)
+            else:
+                print("Nie ma takiego klucza")
+                self.delete()
 
     def pop(self, nodeKey):
         pass
@@ -90,11 +170,25 @@ class Tree:
             order.append(node.val)
         return order
 
+    def delete_by_postorder(self, node):
+        arr = self.postorder(node)
+        print("arr: ", arr)
+        for el in arr:
+            x = self.find(el)
+            self.remove(x)
+            print("deleted: ", el)
+            print("node: ", node)
+
 
 class BSTTree(Tree):
     def __init__(self, src):
         self.root, src = Node(src[0]), [Node(val) for val in src[1:]]
         [self.insert(n, self.root) for n in src]
+        for n in src:
+            if self.root.left:
+                self.root.left.parent = self.root
+            if self.root.right:
+                self.root.right.parent = self.root
 
 
 class AVLTree(Tree):
@@ -120,7 +214,7 @@ class AVLTree(Tree):
 testdata = sorted(rand(10))
 print(sorted(testdata))
 akacja = AVLTree(testdata)
-
+# akacja = BSTTree(testdata)
 print(
     f"testdata :: {testdata}\n",
     akacja.root,
@@ -128,8 +222,16 @@ print(
     "height :: ", akacja.height(_from=akacja.root), '\n',
     "min :: ", akacja.min(_from=akacja.root).val, '\n',
     "max :: ", akacja.max(_from=akacja.root).val, '\n',
+    "path to min :: ", akacja.path_to_min(), '\n',
+    "path to max :: ", akacja.path_to_max(), '\n',
     f"preorder :: {akacja.preorder(akacja.root)}\n",
     f"inorder :: {akacja.inorder(akacja.root)}\n",
     f"postorder :: {akacja.postorder(akacja.root)}\n"
+    f"postorder delete :: {akacja.delete_by_postorder(akacja.root)}\n"
 
 )
+
+
+# usuwanie wybranych elementow:
+akacja.delete()
+print("Drzewo po usunieciu: ", akacja.root)
